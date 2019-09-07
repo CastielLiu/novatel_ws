802906f870b8ad9eb03445c3ed2c4754a91309cb
CastielLiu <castiel_liu@outlook.com>
2019-09-08 00:07:37 +0800

    add nuogeng gps node

---
+++ src/nuogeng/src/nuogeng_node.cpp
@@ -0,0 +1,226 @@
+#include<iostream>
+#include"nuogeng/nuogeng.h"
+
+Nuogeng::Nuogeng():
+	m_reading_status(false),
+	m_pkg_buffer(new uint8_t[500])
+{
+}
+
+Nuogeng::~Nuogeng()
+{
+	this->closeSerial();
+	
+}
+
+bool Nuogeng::openSerial(const std::string& port,int baudrate)
+{
+	m_serial_port = new serial::Serial(port,baudrate,serial::Timeout::simpleTimeout(10)); 
+
+	if (!m_serial_port->isOpen())
+	{
+		std::stringstream output;
+        output << "Serial port: " << port << " failed to open." << std::endl;
+		delete m_serial_port;
+		m_serial_port = NULL;
+		return false;
+	} 
+	else 
+	{
+		std::stringstream output;
+		output << "Serial port: " << port << " opened successfully." << std::endl;
+	}
+
+	m_serial_port->flush();
+	return true;
+}
+
+void Nuogeng::closeSerial()
+{
+	if(m_serial_port!=NULL)
+	{
+		delete m_serial_port;
+		m_serial_port = NULL;
+	}
+		
+}
+
+bool Nuogeng::init(int argc,char** argv)
+{
+	ros::init(argc,argv,"nuogeng_node");
+	ros::NodeHandle nh;
+	ros::NodeHandle nh_private("~");
+	
+	m_pub_id20 = nh.advertise<gps_msgs::Inspvax>(nh_private.param<std::string>("location_topic","/gps"),1);
+	m_pub_id31 = nh.advertise<gps_msgs::Satellites>(nh_private.param<string>("satellite_topic","/satellite"),1);
+	std::string port_name = nh_private.param<std::string>("port_name","/dev/ttyUSB0");
+	int baudrate = nh_private.param<int>("baudrate",115200);
+	if(!openSerial(port_name,baudrate))
+		return false;
+	return true;
+}
+
+void Nuogeng::startReading()
+{
+	if(m_reading_status)
+		return ;
+	m_read_thread_ptr = boost::shared_ptr<boost::thread>(new boost::thread(&Nuogeng::readSerialThread,this));
+}
+
+void Nuogeng::readSerialThread()
+{
+	m_reading_status = true;
+	
+	const int read_size = 200;
+	const int buffer_size = read_size * 2;
+	const int left_reserve_len = 5;
+	uint8_t * const raw_data_buf = new uint8_t[buffer_size+left_reserve_len];
+	uint8_t * const buffer =  raw_data_buf+ left_reserve_len;
+	
+	size_t offset = 0, get_len, total_len;
+		   
+	while(ros::ok() && m_reading_status)
+	{
+		try
+		{
+			get_len = m_serial_port->read(buffer+offset,read_size);
+		}
+		catch(std::exception &e)
+		{
+			ROS_ERROR("Error reading from serial port: %s",e.what());
+		}
+		
+		total_len = offset + get_len;
+		if(total_len < read_size)
+			offset = total_len;
+		else
+		{
+			//cout << total_len << endl;
+			parseIncomingData(buffer, total_len);
+			offset = 0;
+		}
+		boost::this_thread::sleep(boost::posix_time::milliseconds(10));
+	}
+	
+	delete [] raw_data_buf;
+}
+
+void Nuogeng::parseIncomingData(uint8_t* buffer,size_t len)
+{
+	static size_t pkg_buffer_index = 0; //数据包缓存定位索引
+	static size_t pkg_len = 0;			//数据包长度
+	static size_t remainded = 0;		//包头(5bytes)搜索完毕后buffer的剩余长度
+										//remainded <=4
+	//real_buffer指针位于buffer指针之前
+	//上次搜索剩余的数据位于buffer之前
+	//再次搜索时应从real_buffer开始
+	static uint8_t *real_buffer = buffer - remainded;
+	
+	size_t real_buffer_len = len + remainded;
+	remainded = 0; //复位
+	for(size_t i=0; i<real_buffer_len; ++i)
+	{
+		if(pkg_buffer_index ==0) //还未找到数据头
+		{
+			if(real_buffer_len-i > 4 ) //剩余数据可能包含数据头(剩余bytes>=数据头bytes)
+			{
+				if(LRC(real_buffer+i+1,4)==real_buffer[i]) //LRC校验查找数据头
+				{
+					m_pkg_buffer[pkg_buffer_index++] = real_buffer[i];
+					pkg_len = real_buffer[i+2];
+					//cout << pkg_len << endl;
+				}
+			}
+			else if(remainded ==0) // 剩余数据不足且暂未开始拼接
+			{
+				remainded = real_buffer_len-i; //剩余个数
+				//开始拼接，剩余bytes有序放在buffer前面，且保证数据的连续性
+				buffer[i-real_buffer_len] = real_buffer[i];
+			}
+			else
+				buffer[i-real_buffer_len] = real_buffer[i]; //继续拼接
+		}
+		//数据头已经找到，根据包长逐个拷贝(不包括最后一个字节)
+		else if(pkg_buffer_index < pkg_len+4)
+			m_pkg_buffer[pkg_buffer_index++] = real_buffer[i];
+		else
+		{	//拷贝最后一个字节,over
+			m_pkg_buffer[pkg_buffer_index] = real_buffer[i];
+			pkg_buffer_index = 0; //定位索引复位
+			
+			if(m_pkg_buffer[3]+m_pkg_buffer[4]*256 != getCrc16ccittFalseByTable(m_pkg_buffer+5,pkg_len))
+				continue; //校验失败
+				
+			if(20==m_pkg_buffer[1]) //ID
+				parseId20Pkg(m_pkg_buffer);
+			else if(31 == m_pkg_buffer[1])
+				parseId31Pkg(m_pkg_buffer);
+		}
+	}
+}
+
+void Nuogeng::parseId20Pkg(const uint8_t* buffer)
+{
+	auto gpsPtr = (const pkg20Msgs_t *)buffer;
+	
+	m_inspax.header.stamp = ros::Time::now();
+	m_inspax.header.frame_id = "gps";
+	m_inspax.latitude = gpsPtr->latitude *180.0/M_PI;
+	m_inspax.longitude = gpsPtr->longitude *180.0/M_PI;
+	m_inspax.height = gpsPtr->height;
+	m_inspax.north_velocity = gpsPtr->north_velocity;
+	m_inspax.east_velocity = gpsPtr->east_velocity;
+	m_inspax.up_velocity = gpsPtr->down_velocity;
+	m_inspax.roll = gpsPtr->roll;
+	m_inspax.pitch = gpsPtr->pitch;
+	m_inspax.azimuth = gpsPtr->yaw *180.0/M_PI;
+	m_inspax.latitude_standard_deviation = gpsPtr->latitude_std_deviation;
+	m_inspax.longitude_standard_deviation = gpsPtr->longitude_std_deviation;
+	m_inspax.height_standard_deviation = gpsPtr->height_std_deviation;
+	
+	m_pub_id20.publish(m_inspax);
+}
+
+
+void Nuogeng::parseId31Pkg(const uint8_t* buffer)
+{
+	int satellite_count = buffer[2]/7; //pkglen/7
+	gps_msgs::Satellite satellite;
+	gps_msgs::Satellites satellites;
+	
+	satellites.header.stamp = ros::Time::now();
+	satellites.header.frame_id = "gps";
+	satellites.satellites.resize(satellite_count);
+	
+	for(int i=0; i<satellite_count; ++i)
+	{
+		auto satellitePtr = (const pkg31Msgs_t*)(buffer+5+i*7);
+		satellite.navigation_system = satellitePtr->navigation_system;
+		satellite.satellite_num = satellitePtr->satellite_num;
+		satellite.satellite_frequency = satellitePtr->satellite_frequency;
+		satellite.elevation = satellitePtr->elevation;
+		satellite.azimuth = satellitePtr->azimuth;
+		satellite.snr = satellitePtr->snr;
+		
+		satellites.satellites[i] = satellite;
+	}
+	m_pub_id31.publish(satellites);
+}
+
+
+void Nuogeng::stopReading()
+{
+	m_reading_status = false;
+}
+
+
+int main(int argc,char** argv)
+{
+	Nuogeng gps;
+	gps.init(argc,argv);
+	gps.startReading();
+	ros::spin();
+}
+
+
+
